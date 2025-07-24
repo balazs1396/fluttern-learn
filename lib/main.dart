90f1dc62ddf445b3b9b60a22f2b7e4a9dc301b47
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-07-24 21:14:27 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.deepPurple, Colors.purpleAccent], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
