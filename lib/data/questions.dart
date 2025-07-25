fec63d56b7c505a97970f76333ecade1c63a5c19
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-07-25 22:34:39 +0200

    Add quiz dummy data and show it in the first question tab

---
+++ lib/data/questions.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:flutter_first/models/quiz.dart';
+
+const questions = [
+  QuizQuestion('What is the result of 2 + 5?', [
+    '7',
+    '4',
+    '11',
+    '54',
+  ]),
+    QuizQuestion('Questin 2', [
+    'answer 1',
+    'answer 2',
+    'answer 3',
+    'answer 4',
+  ]),
+    QuizQuestion('Questin 3', [
+    'answer 1',
+    'answer 2',
+    'answer 3',
+    'answer 4',
+  ]),
+    QuizQuestion('Questin 4', [
+    'answer 1',
+    'answer 2',
+    'answer 3',
+    'answer 4',
+  ]),
+];
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first/answer_button.dart';
+import 'package:flutter_first/data/questions.dart';
+import 'package:flutter_first/models/quiz.dart';
 
 class QuestionsScreen extends StatefulWidget {
- const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key});
 
   @override
   State<QuestionsScreen> createState() {
@@ -10,11 +13,23 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsState extends State<QuestionsScreen> {
+  final QuizQuestion currentQuestion = questions[0];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Mi van buza"),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.text, style: const TextStyle(color: Colors.white)),
+          const SizedBox(height: 30),
+
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ],
+      ),
     );
   }
 }
